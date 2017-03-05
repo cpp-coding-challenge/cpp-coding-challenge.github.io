24392fd44287a8ffc50d339d49c0fb79d29aa600
zkysar <zach.kysar@gmail.com>
2017-03-04 16:50:25 -0800

    Fix Google Maps location

--- js/gmap.js
+++ js/gmap.js
@@ -1,4 +1,4 @@
-var mapLocation = new google.maps.LatLng(46.200, -72.4310); //change coordinates here
+var mapLocation = new google.maps.LatLng(34.0565328,-117.8237182); //change coordinates here
 var marker;
 var map;
 
@@ -6,17 +6,7 @@ function initialize() {
     var mapOptions = {
         zoom: 12, // Change zoom here
         center: mapLocation,
-        scrollwheel: false,
-        styles: [
-            {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},
-            {"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},
-            {"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
-            {"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},
-            {"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},
-            {"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},
-            {"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-            {"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
-            {"featureType":"water","elementType":"all","stylers":[{"color":"#dbdbdb"},{"visibility":"on"}]}]
+        scrollwheel: false
     };
     
     map = new google.maps.Map(document.getElementById('map'),
@@ -28,7 +18,7 @@ function initialize() {
     + '<div class="map-title">' 
     + '<h3><img alt="" width="140" src="img/brand.png"></h3></div>' 
     + '<p class="map-address"><div class="map-address-row"><i class="fa fa-map-marker"></i><span class="text"><strong>St. Kutuzova</strong><br>Krasnodar, Russian Federation</span></div><div class="map-address-row"><i class="fa fa-phone"></i><span class="text">345 678 91 23</span></div><div class="map-address-row"><span class="map-email"><i class="fa fa-envelope"></i><span class="text">vanessamail@mail.com</span></span></p>' 
-    + '<p class="gmap-open"><a href="https://www.google.com/maps/place/851+6th+Ave,+New+York,+NY+10001,+USA/data=!4m2!3m1!1s0x89c259af44f80211:0xbd87d30d3c7da9d2?sa=X&amp;ei=KqAdVazxJMTkuQS9sIGIBQ&amp;aved=0CB0Q8gEwAA" target="_blank">Open on Google Maps</a></p></div>';
+    + '<p class="gmap-open"><a href="https://www.google.com/maps/place/Cal+Poly+Pomona/@34.0565328,-117.8237182,17z/data=!3m1!4b1!4m5!3m4!1s0x80c32ea5e3dc4361:0x904ae4ed22c91eeb!8m2!3d34.0565284!4d-117.8215295" target="_blank">Open on Google Maps</a></p></div>';
     
     
     var infowindow = new google.maps.InfoWindow({
